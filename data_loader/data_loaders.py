a55242add7b78c9f69e73ecbc6abeb1505682d52
SunQ <sunq0313@gmail.com>
2018-07-06 13:34:05 +0800

    add data set, loader for CUB, COCO

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -4,6 +4,8 @@ import numpy as np
 from torchvision import datasets, transforms
 from base import BaseDataLoader
 
+from datasets import *
+
 
 class SVHNDataLoader(BaseDataLoader):
     def __init__(self, data_dir, batch_size, valid_batch_size=1000, validation_split=0.0, validation_fold=0, shuffle=False, num_workers=4):
@@ -14,8 +16,8 @@ class SVHNDataLoader(BaseDataLoader):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
             # Normalization that every pytorch pretrained models expect
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  std=[0.229, 0.224, 0.225])
+            # transforms.Normalize(mean=[0.485, 0.456, 0.406],
+            #                       std=[0.229, 0.224, 0.225])
         ])
         self.dataset = datasets.SVHN(data_dir, split='train', transform=trsfm, download=True)
         super(SVHNDataLoader, self).__init__(self.dataset, self.batch_size, self.valid_batch_size, shuffle, validation_split, validation_fold, num_workers)
@@ -23,7 +25,6 @@ class SVHNDataLoader(BaseDataLoader):
 
 class CocoDataLoader(BaseDataLoader):
     def __init__(self, data_dir, batch_size, valid_batch_size=1000, validation_split=0.0, validation_fold=0, shuffle=False, num_workers=4):
-
         self.batch_size = batch_size
         self.valid_batch_size = valid_batch_size
             
@@ -31,38 +32,45 @@ class CocoDataLoader(BaseDataLoader):
         self.ann_dir = os.path.join(data_dir, "annotations/captions_train2014.json")
 
         trsfm = transforms.Compose([
-            transforms.CenterCrop((256, 256)),
+            transforms.CenterCrop(256),
+            transforms.Resize(64),
+            transforms.ToTensor(),
+            # Normalization that every pytorch pretrained models expect
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                  std=[0.229, 0.224, 0.225]),
+        ])
+        
+        self.dataset = CocoWrapper(data_dir, transform=trsfm)
+        super(CocoDataLoader, self).__init__(self.dataset, self.batch_size, self.valid_batch_size, shuffle, validation_split, validation_fold, num_workers)
+
+
+class CubDataLoader(BaseDataLoader):
+    def __init__(self, data_dir, batch_size, valid_batch_size=1000, validation_split=0.0, validation_fold=0, shuffle=False, num_workers=4):
+        
+        self.batch_size = batch_size
+        self.valid_batch_size = valid_batch_size
+            
+        trsfm = transforms.Compose([
+            transforms.CenterCrop(256),
+            transforms.Resize(64),
             transforms.ToTensor(),
             # Normalization that every pytorch pretrained models expect
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                   std=[0.229, 0.224, 0.225])
         ])
         
-        self.dataset = datasets.CocoCaptions(self.img_dir, self.ann_dir, transform=trsfm)
-        super(CocoDataLoader, self).__init__(self.dataset, self.batch_size, self.valid_batch_size, shuffle, validation_split, validation_fold, num_workers)
-
-
+        self.dataset = CubDataset(data_dir, transform=trsfm)
+        super(CubDataLoader, self).__init__(self.dataset, self.batch_size, self.valid_batch_size, shuffle, validation_split, validation_fold, num_workers)
 
 if __name__ == '__main__':
-
-    data_dir = '../cocoapi'
-    img_dir = os.path.join(data_dir, "images/train2014")
-    ann_dir = os.path.join(data_dir, "annotations/captions_train2014.json")
-
-    trsfm = transforms.Compose([
-            transforms.CenterCrop((256, 256)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  std=[0.229, 0.224, 0.225]),
-        ])
-
-    dataset = datasets.CocoCaptions(img_dir, ann_dir, transform=trsfm)
-    for index in range(8):
-        data, target = dataset[index]
-        print(target)
+    # coco_loader = CocoDataLoader('../cocoapi', 4)
+    cub_loader = CubDataLoader('../data/birds', 4)
+    # for i, (data, target) in enumerate(cub_loader):
+    #     print(data.shape)
+    #     print(target)
+    #     break
+    
+    for i, (data, target) in enumerate(cub_loader):
         print(data.shape)
-        print(data)
-        print(torch.min(data))
-
-        if index == 7:
-            save_image(data, 'index7.png')
+        print(target)
+        break
--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,34 +1,83 @@
+import sys, os
 import numpy as np
+import pickle as pkl
 import torch
+from PIL import Image
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from torchvision import datasets
+from torchvision import transforms
 
-class My_Dataset(Dataset):
-    def __init__(self, data_dir, transform=None, target_transform=None):
+
+class CubDataset(Dataset):
+
+    def __init__(self, data_dir, transform=None, target_transform=None, train=True):
+        
+        self.image_dir = os.path.join(data_dir, 'CUB_200_2011/images')
+        self.text_dir = os.path.join(data_dir, 'text')
+        
+        self.mode = 'train' if train else 'test'
+        fname_path = os.path.join(data_dir, f'{self.mode}/filenames.pickle')
+        with open(fname_path, 'rb') as fname_file:
+            self.fnames = pkl.load(fname_file)
         self.transform = transform
         self.target_transform = target_transform
-        raise NotImplementedError
-
+        
     def __getitem__(self, idx):
-        data, target = None, None
+        fname = self.fnames[idx]
+        image_path = os.path.join(self.image_dir, f'{fname}.jpg')
+        text_path = os.path.join(self.text_dir, f'{fname}.txt')
+
+        # open image file
+        data = Image.open(image_path)
+        
+        # select one sentence from given set of captions
+        with open(text_path, 'r') as text_file:
+            captions = list(text_file)
+        select_idx = np.random.randint(len(captions), size=None)
+        label = captions[select_idx].replace('\n', '')
+
         if self.transform is not None:
             data = self.transform(data)
-
         if self.target_transform is not None:
-            target = self.target_transform(target)
+            label = self.target_transform(label)
+        return data, label
 
-        raise NotImplementedError
-        return data, target
+    def __len__(self):
+        return len(self.fnames)
 
 
-class CocoWrapper(Dataset):
-    def __init__(self, data_dir):
+class CocoWrapper(datasets.CocoCaptions):
+    def __init__(self, data_dir, transform=None, target_transform=None):
         self.data_dir = data_dir
         self.img_dir = os.path.join(self.data_dir, "images/train2014")
         self.ann_dir = os.path.join(self.data_dir, "annotations/captions_train2014.json")
-        self.coco = datasets.CocoCaptions(self.img_dir, self.ann_dir, transform=trsfm)
+        super(CocoWrapper, self).__init__(self.img_dir, self.ann_dir, transform=transform, target_transform=target_transform)
 
     def __getitem__(self, idx):
-        data, target = self.coco[idx]
-        return data
-    
+        data, target = super(CocoWrapper, self).__getitem__(idx)
+        target = list(target)
+        
+        select_idx = np.random.randint(len(target), size=None)
+        label = target[select_idx].replace('\n', '')
+        return data, label
+
+
+if __name__ == '__main__':
+    trsfm = transforms.Compose([
+        transforms.CenterCrop(256),
+        transforms.Resize(64),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                              std=[0.229, 0.224, 0.225])
+    ])
+    cub_dataset = CubDataset('../data/birds', transform=trsfm, target_transform=None)
+
+    dataloader = DataLoader(cub_dataset, batch_size=24, shuffle=True)
+    index = 10
+    img, target = cub_dataset[index]
+    # for batch, data in enumerate(dataloader):
+    #     img = data
+    #     print(batch, img.shape)
+    print(img.shape)
+    print(target)
